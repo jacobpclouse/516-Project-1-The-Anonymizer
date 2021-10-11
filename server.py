c57ff7bd4e856d0cf69bb8b976dc3999647c0c74
Jacob Clouse <jclouse@albany.edu>
2021-10-11 14:32:47 -0400

    modified the client.py and server.py files so that I have a copy of the original source code and can tinker with features, hoping to be able to send a message back and forth

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket
+'''import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -10,4 +10,16 @@ while True:
     print(f"Connection from {address} has been established.")
     
     
-''' This was taken from https://pythonprogramming.net/sockets-tutorial-python-3/ '''
+ This was taken from https://pythonprogramming.net/sockets-tutorial-python-3/ '''
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+    clientsocket.send(bytes("Hey there!!!","utf-8"))
