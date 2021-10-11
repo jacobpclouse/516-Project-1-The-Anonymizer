5b94049f5638185fab38a608672481f89c1e73a7
Jacob Clouse <jclouse@albany.edu>
2021-10-11 17:42:43 -0400

    added textbook example of UDP client / server code, it looks like it has a few errors in it and I will have to adjust it

---
+++ Test_Files/TextbookClient.py
@@ -0,0 +1,21 @@
+'''from socket import *
+serverName = ’hostname’
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input(’Input lowercase sentence:’)
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+clientSocket.close()'''
+
+
+from socket import *
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.connect((socket.gethostname(), 12000))
+
+message = raw_input('Input lowercase sentence:')
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+clientSocket.close()
--- server.py
+++ server.py
@@ -15,11 +15,12 @@ while True:
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
+s.bind((socket.gethostname(), 1235))
 s.listen(5)
 
 while True:
     # now our endpoint knows about the OTHER endpoint.
     clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
+    print(f"Connection {clientsocket} from {address} has been established.")
     clientsocket.send(bytes("Hey there!!!","utf-8"))
+    clientsocket.close()
