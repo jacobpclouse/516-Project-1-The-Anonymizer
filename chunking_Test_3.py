c2fa3490dd2a9121844a99f11a8cfb49dfabd075
Jacob Clouse <jclouse@albany.edu>
2021-10-20 04:41:23 -0400

    creating copy of chunking to modify and then will move on to udp stop and wait

---
+++ chunking_Test_3.py
@@ -0,0 +1,40 @@
+# abcdefghijklmnopqrstuvwxyz123456789
+
+def chunkerFunction(string):
+    # getting length of string
+    stringLength = len(string)
+    print(f"Length of String: {stringLength} characters")
+
+    # determining the target size (ie: this will be 1000 for udp)
+    byteSize = 10
+
+    # setting a counter equal to length, will decriment as chunks are written
+    lengthLeft = stringLength
+
+    # setting start position variable, will incriment up
+    startCut = 0
+
+    # setting end cut (exclusive), will incriment up
+    endCut = startCut + byteSize
+
+    # implimenting a sort of do while loop
+    # https://www.educative.io/edpresso/how-to-emulate-a-do-while-loop-in-python
+    while True:
+        # printing out chunk equal to byte size
+        print(string[startCut:endCut])
+
+        # Incrimenting startCut and endCut by byteSize
+        startCut += byteSize
+        endCut += byteSize
+
+        # Decrimenting lengthLeft
+        lengthLeft -= byteSize
+
+        # If lengthLeft is less than or equal to byteSize, just print out what is left and end it
+        if(lengthLeft <= byteSize):
+            print(string[startCut:])
+            break
+
+
+message = input("Give us a string to break up: ")
+chunkerFunction(message)
