61ed4340f9fd8a663071c34a69deb13216f900ac
Jacob Clouse <jclouse@albany.edu>
2021-10-20 09:02:01 -0400

    reduced redundant code by using variables instead

--- CommandLoop/command_Loop.py
+++ CommandLoop/command_Loop.py
@@ -16,11 +16,28 @@ userKeywordToCensor = ''
 # https://www.programiz.com/python-programming/methods/string/lower
 while userCommand.lower() != 'quit':
     userCommand = input("What is your command: ")
+    # splitting off 1st three bytes in order to compair
+    userFirst3 = userCommand[0:3].lower()
+    userRemainingCommand = userCommand[4:]
 
-    if userCommand[0:3].lower() == 'put':
-        # Can we assume that the file is in the same directory? Or in a predefined directory?
-        # I am assuming that we only need 'put text.txt' and not 'put /main/docs/My Files/text.txt'
-        # I am slicing based on order, can we expect other orders like text.txt put?
-        userFilePath = userCommand[3:]
+    if len(userCommand) <= 3:
+        # making sure that the user is giving us correct parameters
+        # need to do more checks to confirm that following characters are valid 
+        print("you need to give a valid command")
+    
+    # Check for 'put' Command
+    elif userFirst3 == 'put':
+        # ** Can we assume that the file is in the same directory? Or in a predefined directory?
+        # ** I am assuming that we only need 'put text.txt' and not 'put /main/docs/My Files/text.txt'
+        # ** I am slicing based on order, can we expect other orders like text.txt put?
+        # https://www.w3schools.com/python/ref_func_range.asp
+        userFilePath = userRemainingCommand
+        # cleaning up
+        userCommand = ''
+
+    # Check for 'get' Command
+    elif userFirst3 == 'get':
+        #
+        print(userRemainingCommand)
         # cleaning up
         userCommand = ''
