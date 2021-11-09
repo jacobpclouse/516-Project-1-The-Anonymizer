b47a2c268e8ab44bc79d57bab5135e7988c0e87e
Jacob Clouse <jclouse@albany.edu>
2021-11-09 11:14:24 -0500

    removing old test files

--- Test_Files/UDP_timeout_tests/timeLibTest.py
+++
@@ -1,7 +0,0 @@
-import time
-
-print("Time 1")
-
-time.sleep(3)
-
-print("Time 2")
--- Test_Files/_TCP_Final_v3/OutputText/text.py
+++
@@ -1,8 +0,0 @@
-serverCensoredName = 'anonText.txt'
-FillerText = "This text should be inside the text of anonText"
-
-f = open(f"{serverCensoredName}", "x")
-
-with open(f"{serverCensoredName}", 'w') as f:
-    print(FillerText, file=f)
-
